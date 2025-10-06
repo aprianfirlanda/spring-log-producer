43923acc5ac0b914a5a047eadf9dd0b1ba8e4f15
aprianfirlanda <aprianfirlanda@gmail.com>
2025-10-06 14:09:36 +0700

    first commit

---
+++ src/main/java/com/aprianfirlanda/spring_log_producer/SpringLogProducerApplication.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda.spring_log_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprianfirlanda/spring_log_producer/SpringLogProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprianfirlanda.spring_log_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
